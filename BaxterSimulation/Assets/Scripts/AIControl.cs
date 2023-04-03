1db8cc5b70f319bde73bb00f53b3e0c28f28525e
Edvin Ramirez-Carrillo <eramirez-carrillo@unomaha.edu>
2023-04-03 12:03:58 -0500

    Added navmesh agents to map and also added scripts to help spawn multiple agents at runtime

--- BaxterSimulation/Assets/Scripts/AIControl.cs
+++ BaxterSimulation/Assets/Scripts/AIControl.cs
@@ -5,17 +5,37 @@ using UnityEngine.AI;
 
 public class AIControl : MonoBehaviour {
 
-    public GameObject goal;
+    public GameObject mainEntranceGoal;
+    public GameObject secondEntranceGoal;
     NavMeshAgent agent;
+    Transform target;
+
 
     void Start() {
+        float maindistance = Vector3.Distance(mainEntranceGoal.transform.position, transform.position);
+        float seconddistance = Vector3.Distance(secondEntranceGoal.transform.position, transform.position);
 
+
+
+        if (maindistance < seconddistance)
+        {
+            target = mainEntranceGoal.transform;
+        }
+        else
+        {
+            target = secondEntranceGoal.transform;
+        }
         agent = GetComponent<NavMeshAgent>();
-        agent.SetDestination(goal.transform.position);
+        agent.SetDestination(target.position);
     }
 
 
     void Update() {
+        float distance = Vector3.Distance(target.position, transform.position);
 
+        if (distance <= agent.stoppingDistance)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
---
+++ BaxterSimulation/Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private GameManager _instance;
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(this);
+            Debug.Log("Instance of NetworkManager Created");
+        }
+        else if (_instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public static int MaxAgents = 8000;
+    public static int totalAgents = 0;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        totalAgents = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (totalAgents <= 0)
+        {
+            Application.Quit();
+        }
+    }
+}
